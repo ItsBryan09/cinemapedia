c8ea8c8962743f903dff0053970de90eb35dc559
bryan <bryancarvajal747@gmail.com>
2024-10-25 14:58:46 -0600

    Movies_providers creado

---
+++ lib/presentation/screens/movies/movies_providers.dart
@@ -0,0 +1,28 @@
+import 'package:cinemapedia/domain/entitis/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final nowPlayingMoviesProvider =
+    StateNotifierProvider<MovieNotifier, List<Movie>>((ref) {
+  final fetchMoreMovies = ref.watch(movieRepositoryProvider).getNowPlaying;
+
+  return MovieNotifier(fetchMoreMovies: fetchMoreMovies);
+});
+
+typedef MovieCallback = Future<List<Movie>> Function({int page});
+
+class MovieNotifier extends StateNotifier<List<Movie>> {
+  int currentPage = 0;
+  MovieCallback fetchMoreMovies;
+
+  MovieNotifier({
+    required this.fetchMoreMovies,
+  }) : super([]);
+
+  Future<void> loadNextPage() async {
+    currentPage++;
+
+    final List<Movie> movies = await fetchMoreMovies(page: currentPage);
+    state = [...state, ...movies];
+  }
+}
