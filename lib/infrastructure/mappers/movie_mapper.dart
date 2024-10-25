497d427ecb853801ada96ead0b377a820a6c3a74
bryan <bryancarvajal747@gmail.com>
2024-10-24 18:20:51 -0600

    Mapper Agregado

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,24 @@
+import 'package:cinemapedia/domain/entitis/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
+      adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
+          : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSktdnGhkcnHjEnC21GEbjeiLQef0m-5YC9sfe3BD09b8to6ndNvp-7bkJBhgyaDiLzhFw&usqp=CAU',
+      genreIds: moviedb.genreIds.map((e) => e.toString()).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+          ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
+          : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSktdnGhkcnHjEnC21GEbjeiLQef0m-5YC9sfe3BD09b8to6ndNvp-7bkJBhgyaDiLzhFw&usqp=CAU',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount);
+}
