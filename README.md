53ddd763acbc2d5010a25781f01edb741cbdaca5
bryan <bryancarvajal747@gmail.com>
2024-11-02 13:50:52 -0600

    implementacion de Isar DataBase

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 
 1.Copiar el .env.templete y renombrar a .env
 2. Cambiar las variables de entorno (The MoviDB)
+3. cambios en la entidad, hay que ejecutar el comando => flutter pub run build_runner build
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,105 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
-import '../../providers/providers.dart';
+import 'package:cinemapedia/presentation/views/views.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
-  const HomeScreen({super.key});
+  final int pageIndex;
+  const HomeScreen({super.key, required this.pageIndex});
+
+  final viewRoutes = const <Widget>[HomeView(), SizedBox(), FavoritesView()];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView(),
-      bottomNavigationBar: CustomBottomNavigation(),
-    );
-  }
-}
-
-class _HomeView extends ConsumerStatefulWidget {
-  const _HomeView();
-
-  @override
-  _HomeViewState createState() => _HomeViewState();
-}
-
-class _HomeViewState extends ConsumerState<_HomeView> {
-  @override
-  void initState() {
-    super.initState();
-
-    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-    ref.read(popularMoviesProvider.notifier).loadNextPage();
-    ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-    ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final initialLoading = ref.watch(initialLoadingProvider);
-
-    if (initialLoading) return const FullScreenLoader();
-
-    final slideshowMovies = ref.watch(moviesSlideshowPrivider);
-    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    final popularMovies = ref.watch(popularMoviesProvider);
-    final upcomingMovies = ref.watch(upcomingMoviesProvider);
-    final topRatedMovies = ref.watch(topRatedMoviesProvider);
-
-    return CustomScrollView(slivers: [
-      const SliverAppBar(
-        floating: true,
-        flexibleSpace: FlexibleSpaceBar(
-          centerTitle: true,
-          title: CustomAppbar(),
-        ),
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: viewRoutes,
       ),
-      SliverList(
-          delegate: SliverChildBuilderDelegate((context, index) {
-        return Column(
-          children: [
-            // const CustomAppbar(),
-
-            MoviesSlideshow(movies: slideshowMovies),
-
-            MovieHorizontalListview(
-              movie: nowPlayingMovies,
-              title: 'En cine',
-              subTitle: 'Lunes 20',
-              loadNextPage: () {
-                ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movie: upcomingMovies,
-              title: 'Proximamente',
-              subTitle: 'En este mes',
-              loadNextPage: () {
-                ref.read(upcomingMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movie: popularMovies,
-              title: 'Populares',
-              //subTitle: 'En este mes',
-              loadNextPage: () {
-                ref.read(popularMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            MovieHorizontalListview(
-              movie: topRatedMovies,
-              title: 'Mejor calificadas',
-              //subTitle: 'Desde siempre',
-              loadNextPage: () {
-                ref.read(topRatedMoviesProvider.notifier).loadNextPage();
-              },
-            ),
-            const SizedBox(
-              height: 20,
-            )
-          ],
-        );
-      }, childCount: 1))
-    ]);
+      bottomNavigationBar: CustomBottomNavigation(
+        currentIndex: pageIndex,
+      ),
+    );
   }
 }
