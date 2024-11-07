ef7255f09ab12c2318f8eed7bc5e3375f9933bd4
bryan <bryancarvajal747@gmail.com>
2024-11-07 12:48:52 -0600

    interaccion con la base de datos

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:cinemapedia/domain/entitis/movie.dart';
-import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
+
 import 'package:cinemapedia/presentation/providers/providers.dart';
 
 class MovieScreen extends ConsumerStatefulWidget {
@@ -188,12 +188,12 @@ class _ActorsByMovie extends ConsumerWidget {
   }
 }
 
-class _CustomSliverAppBar extends StatelessWidget {
+class _CustomSliverAppBar extends ConsumerWidget {
   final Movie movie;
   const _CustomSliverAppBar({required this.movie});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
 
     return SliverAppBar(
@@ -203,7 +203,7 @@ class _CustomSliverAppBar extends StatelessWidget {
       actions: [
         IconButton(
             onPressed: () {
-              //todo realizar el toggle
+              ref.watch(localStorageRepositoryProvider).toggleFavorite(movie);
             },
             icon: const Icon(Icons.favorite_border)
             //icon: const Icon(Icons.favorite_rounded,color: Colors.red,)
